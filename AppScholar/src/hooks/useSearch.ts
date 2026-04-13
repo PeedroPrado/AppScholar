fb0c44d5253c4e7bd1433162887c0dc8af894185
Pedro H Prado <pedrohenriquepradodenovaes@gmail.com>
2026-04-12 23:48:56 -0300

    Refactor useSearch function formatting

--- AppScholar/src/hooks/useSearch.ts
+++ AppScholar/src/hooks/useSearch.ts
@@ -6,7 +6,7 @@ export function useSearch<T>(items: T[], keys: (keyof T)[]) {
   const [search, setSearch] = useState('');
 
   const filtered = items.filter(item =>
-    // some() retorna true se PELO MENOS UMA das chaves bater com a busca
+    
     keys.some(key =>
       String(item[key]).toLowerCase().includes(search.toLowerCase())
     )
